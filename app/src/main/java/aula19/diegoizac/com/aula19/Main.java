94af02ebc16a85ba57c27fc0d2d71c40786a89e2
diegoizac <diegoizac@hotmail.com>
2017-04-24 13:58:38 -0300

    Initial commit

---
+++ app/src/main/java/aula19/diegoizac/com/aula19/Main.java
@@ -0,0 +1,14 @@
+
+package aula19.diegoizac.com.aula19;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
